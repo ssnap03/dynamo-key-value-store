a03318323cc1b208e5483f695d9c47f270c68ee0
Anirudh Nistala <ani.nistala@gmail.com>
2024-05-01 14:21:47 -0400

    adds experiment results

--- apps/dynamo/test/experiment4.exs
+++ apps/dynamo/test/experiment4.exs
@@ -26,8 +26,8 @@ defmodule DynamoTest do
             client = Dynamo.Client.new_client(:client, :a) 
             end)
         
-        {count_b, count_c, count_e} = check_eventual_consistency(client, view, 0, 0, 0, 0)
-        IO.puts("count_b = #{inspect(count_b)}; count_c = #{inspect(count_c)}; count_e = #{inspect(count_e)}")
+        {count_b, count_c, count_e, count_d} = check_eventual_consistency(client, view, 0, 0, 0, 0, 0)
+        IO.puts("count_b = #{inspect(count_b)}; count_c = #{inspect(count_c)}; count_e = #{inspect(count_e)}; count_d = #{inspect(count_d)}")
         
         handle = Process.monitor(client)
         # Timeout.
@@ -41,14 +41,15 @@ defmodule DynamoTest do
       Emulation.terminate()
     end
 
-    def check_eventual_consistency(client, view, loopi, count_b, count_c, count_e) do
-        {count_b, count_c, count_e} =
-            if loopi >= 10 do
-                {count_b, count_c, count_e}
+    def check_eventual_consistency(client, view, loopi, count_b, count_c, count_e, count_d) do
+        {count_b, count_c, count_e, count_d} =
+            if loopi >= 25 do
+                {count_b, count_c, count_e, count_d}
             else
                 Dynamo.Client.kill(client, :b)
                 Dynamo.Client.kill(client, :c)
                 Dynamo.Client.kill(client, :e)
+                Dynamo.Client.kill(client, :d)
 
                 receive do 
                     after 4_000 ->          
@@ -62,19 +63,56 @@ defmodule DynamoTest do
                 Dynamo.Client.revive(client, :b)
                 Dynamo.Client.revive(client, :c)
                 Dynamo.Client.revive(client, :e)
+                Dynamo.Client.revive(client, :d)
 
-                Process.sleep(10000)
+                Process.sleep(10)
+                # 0.72, 0.88, 0.96, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0
+                # 10ms -> 0.72 r=1 w=1 f=1
+                # 100ms -> 0.88 r=1 w=1 f=1
+                # 500ms -> 0.96 r=1 w=1 f=1
+                # 1000ms -> 25 r=1 w=1 f=1
+                # 1500ms -> 25 r=1 w=1 f=1
+                # 2000ms -> 25 r=1 w=1 f=1
+                # 2500ms -> 25 r=1 w=1 f=1
+                # 3000ms -> 25 r=1 w=1 f=1
+                # 4000ms -> 25 r=1 w=1 f=1
+                # 5000ms -> 25 r=1 w=1 f=1
 
-                # 5ms -> 4,4,7 r=1 w=1 n=5
-                # 400ms -> 6,5,8 r=1 w=1 n=5
-                # 800ms -> 8,8,7 r=1 w=1 n=5
-                # 1500ms -> 9,8,7 r=1 w=1 n=5
-                # 1800ms -> 9,10,10 r=1 w=1 n=5
-                # 2000ms -> 8,7,10 r=1 w=1 n=5
-                # 2500ms -> 10,9,10 r=1 w=1 n=5
-                # 3000ms -> 8,6,9 r=1 w=1 n=5
-                # 4000ms -> 10,10,10 r=1 w=1 n=5
-                # 5000ms -> 9,10,9 r=1 w=1 n=5
+                # 0.9, 0.9, 0.94, 0.94, 0.96, 1.0, 1.0, 1.0, 1.0, 1.0 
+                # 10ms -> 23,22 r=1 w=1 f=2
+                # 100ms -> 23,22 r=1 w=1 f=2
+                # 500ms -> 24, 23 r=1 w=1 f=2
+                # 1000ms -> 24,23 r=1 w=1 f=2
+                # 1500ms -> 24,24 r=1 w=1 f=2
+                # 2000ms -> 25, 25 r=1 w=1 f=2
+                # 2500ms -> 25 r=1 w=1 f=2
+                # 3000ms -> 25 r=1 w=1 f=2
+                # 4000ms -> 25 r=1 w=1 f=2
+                # 5000ms -> 25 r=1 w=1 f=2
+
+                # 0.97, 0.96, 0.96, 0.96, 0.94, 0.97, 0.97, 1.0, 1.0, 1.0
+                # 10ms -> 24, 24, 25 r=1 w=1 f=3
+                # 100ms -> 24, 23, 25 r=1 w=1 f=3
+                # 500ms -> 24, 24, 24 r=1 w=1 f=3
+                # 1000ms -> 22, 25, 25 r=1 w=1 f=3
+                # 1500ms -> 24, 22, 24 r=1 w=1 f=3
+                # 2000ms -> 24, 24, 25 r=1 w=1 f=3
+                # 2500ms -> 25, 24, 24 r=1 w=1 f=3
+                # 3000ms -> 25, 25, 25 r=1 w=1 f=3
+                # 4000ms -> 25, 25, 25 r=1 w=1 f=3
+                # 5000ms -> 25, 25, 25 r=1 w=1 f=3
+
+                # 0.9, 0.86, 0.89, 0.96, 0.98, 0.98, 1.0, 0.98, 1.0, 0.98
+                # 10ms -> 22, 22, 24, 22 r=1 w=1 f=4
+                # 100ms -> 21, 21, 22, 22 r=1 w=1 f=4
+                # 500ms -> 22, 22, 24, 21 r=1 w=1 f=4
+                # 1000ms -> 24, 24, 25, 23 r=1 w=1 f=4
+                # 1500ms -> 24, 24, 25, 25 r=1 w=1 f=4
+                # 2000ms -> 24, 25, 24, 25 r=1 w=1 f=4
+                # 2500ms -> 25, 25, 25, 25 r=1 w=1 f=4
+                # 3000ms -> 24, 25 , 25, 24 r=1 w=1 f=4
+                # 4000ms -> 25, 25, 25, 25 r=1 w=1 f=4
+                # 5000ms -> 24, 24, 25, 25 r=1 w=1 f=4
 
 
                 Dynamo.Client.get(client, :b, "a")
@@ -107,11 +145,21 @@ defmodule DynamoTest do
                         end
                 end
 
-                check_eventual_consistency(client, view, loopi+1, count_b, count_c, count_e)
+                Dynamo.Client.get(client, :d, "a")
+                count_d = receive do 
+                    {:get, key, vals, :d} -> 
+                        if Enum.at(vals, 0) == "100" do
+                            count_d+1
+                        else
+                            count_d
+                        end
+                end
+
+                check_eventual_consistency(client, view, loopi+1, count_b, count_c, count_e, count_d)
 
             end
             
-        {count_b, count_c, count_e}
+        {count_b, count_c, count_e, count_d}
     end
     
   end
